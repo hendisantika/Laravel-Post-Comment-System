a4a5c325c9f0958b52d95f291768faa8a1fd3829
hendisantika <hendisantika@yahoo.co.id>
2023-11-15 12:06:49 +0700

    #15 - Add index in PostController 🌀✏️📗🐧🐳

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,10 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
+
 class PostController extends Controller
 {
     public function __construct()
     {
         $this->middleware('auth');
+    }
+
+    public function index()
+    {
+        $posts = Post::take(5)->get();
+
+        return view('post.index', compact('posts'));
     }
 }
